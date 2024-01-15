c57b17409714277f989573e1141081e423bf4069
mohamedelropy <154741292+mohamedelropy@users.noreply.github.com>
2024-01-15 02:36:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let navbar = document.querySelector(".navbar");
 let logo = document.querySelector(".navbar-brand");
 let navbarlinks = document.querySelectorAll(".nav-item a");
+let activelink = document.querySelector(".navbar-nav .active");
 let icon = document.querySelector("#icon .fa-solid");
 let phone = document.querySelector(".btn");
 
@@ -8,9 +9,11 @@ document.addEventListener('scroll', function() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 ) {
         navbar.style.position = "fixed";
         navbar.style.width = "90%";
-        navbar.style.marginTop = "1%"
-        navbar.style.marginLeft = "5%"
-        navbar.style.marginRight = "5%"
+        navbar.style.paddingTop = "0.5rem";
+        navbar.style.marginTop = "1%";
+        navbar.style.marginLeft = "5%";
+        navbar.style.borderRadius = "0.5rem";
+        navbar.style.marginRight = "5%";
         navbar.style.backgroundColor = "white";
         logo.style.color = "black";
         icon.style.backgroundColor = "#16B789";
@@ -20,11 +23,13 @@ document.addEventListener('scroll', function() {
 
         for (let i = 0 ; i < navbarlinks.length; i++ ){
             navbarlinks[i].style.color = "black";
+            navbarlinks[0].style.color = "#16B789";
         }
     }else {
         navbar.style.position = "relative";
         navbar.style.backgroundColor = "transparent";
         navbar.style.width = "100%";
+        navbar.style.paddingTop = "1.5rem";
         navbar.style.marginTop = "0"
         navbar.style.marginLeft = "0"
         navbar.style.marginRight = "0"
@@ -39,6 +44,28 @@ document.addEventListener('scroll', function() {
 
         for (let i = 0; i < navbarlinks.length; i++) {
             navbarlinks[i].style.color = "white";
+            navbarlinks[0].style.color = "#16B789";
         }
     }
 })
+
+let sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id")
+
+        if (top >= offset && top < offset + height ) {
+            navbarlinks.forEach(link => {
+                link.classList.remove("active");
+            })
+            document.querySelector(`.nav-item a[href = '#${id}']`).classList.add('active');
+        }
+        
+    } ) 
+    
+})
+
